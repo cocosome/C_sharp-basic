98ffabe1196922b40d5aae6428d3c32a45100485
cocosome <kcsgaram123@gmail.com>
2024-11-17 09:27:16 +0900

    - using : using abc 선언시, abc의 하위 keyword를 사용할 때 abc를 생략하고 쓸 수 있다. - WriteLine()의 큰 따옴표 내부에 적힌 {0}, {1}는 C언어에서 %d와 비슷한 역할을 하는것으로 추정된다.

---
+++ 20241117/Hello/MainApp.cs
@@ -0,0 +1,20 @@
+﻿using System; // System 하위의 keyword를 사용할 수 있게됨, ex) Console.WriteLine()... , 그러나 그냥 WriteLine()은 사용불가
+using static System.Console; // Console 하위의 Keyword를 사용할 수 있게됨, ex) WriteLine()...
+//using ~~~를 적음으로써 해당 ~~~ keyword를 생략할 수 있게됨
+
+namespace Hello
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                WriteLine("사용법 : Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hello, {0} {1}", args[0], args[1]); // {0}이라고 적으면 args[0]에 있는 값을 가져와서 출력함, C언어의 %d랑 비슷한 역할 수행
+        }
+    }
+}
